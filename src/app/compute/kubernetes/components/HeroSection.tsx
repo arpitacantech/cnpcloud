c941411ffeac869b66480cf4092a8147198080ad
hardik-cn <hardik.cantech@gmail.com>
2025-12-24 18:58:34 +0530

    Update HeroSection.tsx

--- src/app/compute/kubernetes/components/HeroSection.tsx
+++ src/app/compute/kubernetes/components/HeroSection.tsx
@@ -1,37 +1,3 @@
-// "use client";
-// import Image from "next/image";
-
-// export default function HeroSection() {
-//   return (
-//     <section className="bg-[#09090a] border-y border-base-900">
-//         <div className="max-w-screen-xl mx-auto px-4 pt-[180px] pb-0 gap-12 md:px-8">
-//             <div className="space-y-5 max-w-6xl mx-auto text-center">
-//                 <h1 className="text-sm text-white font-medium rounded-[114px] opacity-100 inline-block p-[7px_15px] bg-[#121212] rounded-[26px] border border-[#383838] inline-block">
-//                     Cantech Cloud Kubernetes
-//                 </h1>
-//                 <h2 className="text-[46px] text-white font-extrabold mx-auto">
-//                     Highly Available Managed Kubernetes with Control Plane
-//                 </h2>
-//                 <p className="max-w-2xl mx-auto pb-4 text-[#ffffffb3]">
-//                     Focus on building, scaling, and deploying applications without worrying about infrastructure management.
-//                 </p>
-//                 <div className="items-center justify-center gap-x-3 space-y-3 sm:flex sm:space-y-0">
-//                     <a href="javascript:void(0)" className="bg-white text-black px-6 py-3 rounded-full font-medium transition-all duration-300 ease-in-out shadow-md hover:shadow-[0_0_20px_rgba(255,255,255,0.8)] hover:scale-105">
-//                         Get Started
-//                     </a>
-//                     {/* <a href="javascript:void(0)" className="block py-2 px-4 text-gray-700 hover:text-gray-500 font-medium duration-150 active:bg-gray-100 border rounded-lg">
-//                         Get access
-//                     </a> */}
-//                 </div>
-//             </div>
-//             <div className="pt-[60px]">
-//                 <img src="https://upstart-astro.netlify.app/_astro/product-demo.Dz7pFNHq_Z21TbDb.webp" className="w-full shadow-lg" alt="" />
-//                 {/* /assets/images/compute/kubernetes/original-c456c48556965c725ac02caa41942938.png */}
-//             </div>
-//         </div>
-//     </section>
-//   )
-// }
 "use client";
 
 import { Cloud } from 'lucide-react';
