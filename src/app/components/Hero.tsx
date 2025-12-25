02b91675ae3b21681115516252fdb6b43f7c92bf
hardik-cn <hardik.cantech@gmail.com>
2025-12-25 17:58:41 +0530

    Update Hero.tsx

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -12,16 +12,18 @@ export default function Hero() {
         <div className="relative z-10 flex h-full flex-col items-center justify-center px-4 text-center sm:px-6 md:px-10 lg:px-20 -translate-y-4 md:-translate-y-10">
           <h1 className="cc-h1">
             <span className="block bg-gradient-to-b from-white via-gray-300 to-gray-400 bg-clip-text text-transparent">
-              Power Your Future With Scalable
+              India's Best Cloud Compute
             </span>
             <span className="block bg-gradient-to-b from-white via-gray-300 to-gray-400 bg-clip-text text-transparent md:mt-3">
-              Cloud Solution
+              and PaaS Solutions
             </span>
           </h1>
           <ul className="list-none mb-14 flex flex-col md:flex-row md:items-center md:justify-center md:flex-wrap lg:flex-nowrap gap-4 md:gap-9 w-fit md:mx-auto">
             <li data-ns-animate="" data-delay="0.3" className="flex items-center gap-2.5 opacity-100 blur-0 translate-x-0 translate-y-0 rotate-0 scale-100">
               <span className="bg-secondary rounded-full p-1"><Check size={16} /></span>
-              <span className="text-tagline-2 dark:text-accent/60"> Fast Deployment </span>
+              <span className="text-tagline-2 dark:text-accent/60">
+               Fast Deployment
+              </span>
             </li>
             <li data-ns-animate="" data-delay="0.4" className="flex items-center gap-2.5 opacity-100 blur-0 translate-x-0 translate-y-0 rotate-0 scale-100">
               <span className="bg-secondary rounded-full p-1"><Check size={16} /></span>
@@ -31,11 +33,15 @@ export default function Hero() {
             </li>
             <li data-ns-animate="" data-delay="0.5" className="flex items-center gap-2.5 opacity-100 blur-0 translate-x-0 translate-y-0 rotate-0 scale-100">
               <span className="bg-secondary rounded-full p-1"><Check size={16} /></span>
-              <span className="text-tagline-2 dark:text-accent/60"> 99.99% Uptime </span>
+              <span className="text-tagline-2 dark:text-accent/60"> 
+              99.99% Uptime
+            </span>
             </li>
             <li data-ns-animate="" data-delay="0.5" className="flex items-center gap-2.5 opacity-100 blur-0 translate-x-0 translate-y-0 rotate-0 scale-100">
               <span className="bg-secondary rounded-full p-1"><Check size={16} /></span>
-              <span className="text-tagline-2 dark:text-accent/60"> Secure & Reliable </span>
+              <span className="text-tagline-2 dark:text-accent/60"> 
+                Secure & Reliable
+              </span>
             </li>
           </ul>
         </div>
