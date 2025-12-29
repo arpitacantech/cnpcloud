2eb12b936c42fb0c27c02916d78794e13e0ffcba
hardik-cn <hardik.cantech@gmail.com>
2025-12-29 18:52:08 +0530

    Update page.tsx

--- src/app/paas/application/laravel-hosting/page.tsx
+++ src/app/paas/application/laravel-hosting/page.tsx
@@ -4,8 +4,8 @@ import Features from "./components/Features";
 import CardSection from "./components/CardSection";
 import KeyFeatures from "./components/KeyFeatures";
 import ComparisonSection from "@/src/app/components/ComparisonSection";
-import CTASection from "@/src/app/components/CTASection";
-import FAQ1 from "@/src/app/components/FAQ1";
+import CTASection from "./components/CTASection";
+import Faq from "./components/Faq";
 import Footer from "@/src/app/components/Footer";
 
 
@@ -19,7 +19,7 @@ export default function LaravelHosting() {
   <ComparisonSection />
     <KeyFeatures />
     <CTASection />
-  <FAQ1 />
+  <Faq />
   
   <Footer />
   </>
