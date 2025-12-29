b6393646880915f051f5f8fad3a5aa616be5fd12
hardik-cn <hardik.cantech@gmail.com>
2025-12-29 11:26:43 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import type { Metadata } from "next";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import TwoCardSection from "./components/TwoCardSection";
@@ -8,6 +9,24 @@ import ComparisonSection from "./components/ComparisonSection";
 import FAQ1 from "./components/FAQ1";
 import CTASection from "./components/CTASection";
 import Footer from "./components/Footer";
+
+export const metadata: Metadata = {
+  title: "Cloud Compute & PaaS Solutions in India | Cantech Cloud",
+  description: "Run modern workloads on Cantech Cloud with reliable cloud compute and PaaS services. Flexible infrastructure, optimized performance, and cost control.",
+  keywords: [
+    "Cloud Compute",
+    "PaaS Solutions",
+  ],
+  openGraph: {
+    title: "Cloud Compute & PaaS Solutions in India | Cantech Cloud",
+    url: "https://www.cantech.cloud/",
+    description: "Run modern workloads on Cantech Cloud with reliable cloud compute and PaaS services. Flexible infrastructure, optimized performance, and cost control.",
+  },
+
+  alternates: {
+    canonical: "https://www.cantech.cloud/",
+  },
+};
 
 export default function Home() {
   return (
