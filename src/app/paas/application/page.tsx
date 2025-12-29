6539acd9bc50380e28c40b408b7fd5e3de3dd1eb
hardik-cn <hardik.cantech@gmail.com>
2025-12-29 15:52:34 +0530

    Update page.tsx

--- src/app/paas/application/page.tsx
+++ src/app/paas/application/page.tsx
@@ -1,11 +1,11 @@
 import PaasNavbar from "../components/PaasNavbar";
 import HeroSection from "./components/HeroSection";
 import ApplicationSection from "./components/ApplicationSection";
-import DifferentiationSection from "../components/DifferentiationSection";
+import DifferentiationSection from "./components/DifferentiationSection";
 import ComparisonSection from "../../components/ComparisonSection";
 import CTASection from "../../components/CTASection";
 import Footer from "../../components/Footer";
-import FAQ1 from "../../components/FAQ1";
+import FAQ1 from "./components/FAQ1";
 
 export default function paas() {
   return (
