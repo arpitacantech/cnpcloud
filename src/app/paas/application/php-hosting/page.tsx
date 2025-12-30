3a09ec70c38fee072ede8a5c6c7bb85289c4ffff
hardik-cn <hardik.cantech@gmail.com>
2025-12-30 12:07:18 +0530

    Update page.tsx

--- src/app/paas/application/php-hosting/page.tsx
+++ src/app/paas/application/php-hosting/page.tsx
@@ -1,26 +1,25 @@
-import PaasNavbar from "../../components/PaasNavbar";
+import Navbar from "@/src/app/components/Navbar";
 import HeroInside from "./components/HeroInside";
-import DifferentiationSection from "@/src/app/paas/components/DifferentiationSection";
-import ScrollCards from "@/src/app/paas/components/ScrollCards";
-import KeyFeatures from "../laravel-hosting/components/KeyFeatures";
+import DifferentiationSection from "./components/DifferentiationSection";
+import ScrollCards from "./components/ScrollCards";
+import KeyFeatures from "./components/KeyFeatures";
 import ComparisonSection from "@/src/app/components/ComparisonSection";
-import CTASection from "@/src/app/components/CTASection";
-import FAQ1 from "@/src/app/components/FAQ1";
+import CTASection from "./components/CTASection";
+import Faq from "./components/Faq";
 import Footer from "@/src/app/components/Footer";
-
 
 export default function paas() {
   return (
     <>
-      <PaasNavbar />
+      <Navbar />
       <HeroInside />
       <DifferentiationSection />
       <ScrollCards />
       <ComparisonSection />
-        <KeyFeatures />
-        <CTASection />
-        <FAQ1 />    
-        <Footer />
+      <KeyFeatures />
+      <Faq />
+      <CTASection />   
+      <Footer />
     </>
   );
 }
