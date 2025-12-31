1770f1392333628da7959673bf96b8c9a1ebde3e
hardik-cn <hardik.cantech@gmail.com>
2025-12-31 13:33:47 +0530

    Update page.tsx

--- src/app/paas/page.tsx
+++ src/app/paas/page.tsx
@@ -4,7 +4,7 @@ import HeroSection from "@/src/app/paas/components/HeroSection";
 import DifferentiationSection from "./components/DifferentiationSection";
 import ScrollCards from "./components/ScrollCards";
 import Integration from "./components/Integration";
-import CTASection from "../components/CTASection";
+import FooterCTA from "../components/FooterCTA";
 import Footer from "../components/Footer";
 import FAQ1 from "./components/FAQ1";
 
@@ -17,7 +17,13 @@ export default function paas() {
   <ScrollCards />
   <Integration />
   <FAQ1 />
-  <CTASection />
+  <FooterCTA
+    titleLine1="Need Some"
+    titleLine2="Professional Help?"
+    description="Contact our Customer Service representative to provide you with the best custom made solutions on the market."
+    // buttonText="Let's Talk"
+    // buttonLink="/contact"
+  />
   <Footer />
   </>
   );
