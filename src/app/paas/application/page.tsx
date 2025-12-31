f459f53fa2f526fd8afc1d9f68cf68ddaf16fba1
hardik-cn <hardik.cantech@gmail.com>
2025-12-31 17:57:40 +0530

    Update page.tsx
    meta content added

--- src/app/paas/application/page.tsx
+++ src/app/paas/application/page.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from "next";
 import PaasNavbar from "../components/PaasNavbar";
 import HeroSection from "./components/HeroSection";
 import ApplicationSection from "./components/ApplicationSection";
@@ -6,6 +7,35 @@ import ComparisonSection from "../../components/ComparisonSection";
 import CTASection from "../../components/CTASection";
 import Footer from "../../components/Footer";
 import FAQ1 from "./components/FAQ1";
+
+export const metadata: Metadata = {
+  title: "Best Platform for PaaS Application Hosting | Cantech Cloud",
+  description: "One platform for all your PaaS applications. Cantech PaaS makes deployment simple, secure and infinitely scalable with managed infrastructure and one-click app hosting.",
+  keywords: [
+    "PaaS Application",
+    "Application Hosting",
+  ],
+   
+  openGraph: {
+    title: "Best Platform for PaaS Application Hosting | Cantech Cloud",
+    siteName: "Cantech Cloud",
+    url: "https://www.cantech.cloud/application",
+    description: "One platform for all your PaaS applications. Cantech PaaS makes deployment simple, secure and infinitely scalable with managed infrastructure and one-click app hosting.",
+    type: "website",
+    images: [
+      {
+        url: "/assets/images/og_image.png",
+        width: 1200,
+        height: 630,
+        alt: "Cantech Cloud",
+      },
+    ],
+  },
+
+  alternates: {
+    canonical: "https://www.cantech.cloud/application",
+  },
+};
 
 export default function paas() {
   return (
