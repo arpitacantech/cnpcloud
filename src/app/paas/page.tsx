77fe7ccf0604066285ca4246da030409023e73e8
hardik-cn <hardik.cantech@gmail.com>
2025-12-31 17:54:40 +0530

    Update page.tsx
    site meta detail added

--- src/app/paas/page.tsx
+++ src/app/paas/page.tsx
@@ -1,4 +1,5 @@
 
+import type { Metadata } from "next";
 import PaasNavbar from "./components/PaasNavbar";
 import HeroSection from "@/src/app/paas/components/HeroSection";
 import DifferentiationSection from "./components/DifferentiationSection";
@@ -7,6 +8,36 @@ import Integration from "./components/Integration";
 import FooterCTA from "../components/FooterCTA";
 import Footer from "../components/Footer";
 import FAQ1 from "./components/FAQ1";
+
+export const metadata: Metadata = {
+  title: "scalable PaaS & cloud Compute with Cantech Cloud",
+  description: "Cantech Cloud is a scalable cloud platform offering compute, PaaS, Kubernetes, storage and secure high-performance infrastructure for modern applications & enterprises.",
+  keywords: [
+    "PaaS",
+    "Platform As a Service",
+    "PaaS Cloud Platform",
+  ],
+   
+  openGraph: {
+    title: "scalable PaaS & cloud Compute with Cantech Cloud",
+    siteName: "Cantech Cloud",
+    url: "https://www.cantech.cloud/paas",
+    description: "Cantech Cloud is a scalable cloud platform offering compute, PaaS, Kubernetes, storage and secure high-performance infrastructure for modern applications & enterprises.",
+    type: "website",
+    images: [
+      {
+        url: "/assets/images/og_image.png",
+        width: 1200,
+        height: 630,
+        alt: "Cantech Cloud",
+      },
+    ],
+  },
+
+  alternates: {
+    canonical: "https://www.cantech.cloud/paas",
+  },
+};
 
 export default function paas() {
   return (
