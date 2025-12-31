c8e33f4f401ee141b68bcc98adfd60d91710f02f
arpitacantech <arpita.cantech@gmail.com>
2025-12-31 15:19:59 +0530

    Refactor FAQ1 component kubernetes

--- src/app/compute/kubernetes/components/FAQ1.tsx
+++ src/app/compute/kubernetes/components/FAQ1.tsx
@@ -205,27 +205,21 @@ useEffect(() => {
                       <h3 className="cc-h3">{item.question}</h3>
                     </div>
                     
-  <div
-  id={panelId}
-  role="region"
-  aria-labelledby={buttonId}
-  className={`grid transition-[grid-template-rows,opacity] duration-500 ease-out ${
-    open ? "grid-rows-[1fr] opacity-100" : "grid-rows-[0fr] opacity-0"
-  } text-neutral-400`}
->
-  <div className="overflow-hidden">
-    <div className="cc-p pt-4">
-      {Array.isArray(item.answer) ? (
-        <ul className="list-disc pl-5 space-y-2">
-          {item.answer.map((point, i) => (
-            <li key={i}>{point}</li>
-          ))}
-        </ul>
-      ) : (
-        <p>{item.answer}</p>
-      )}
-    </div>
-  </div>
+<div
+                      id={panelId}
+                      role="region"
+                      aria-labelledby={buttonId}
+                      className={`overflow-hidden text-lg leading-relaxed transition-[max-height] duration-500 ease-out ${open ? "max-h-64" : "max-h-0"} text-neutral-400`}>
+                      <div className="cc-p pt-4">
+  {Array.isArray(item.answer) ? (
+    <ul className="list-disc pl-5 space-y-2">
+      {item.answer.map((point, i) => (
+        <li key={i}>{point}</li>
+      ))}
+    </ul>
+  ) : (
+    <p>{item.answer}</p>
+  )}
 </div>
 
 
@@ -244,5 +238,6 @@ useEffect(() => {
 
 export default FAQ1;
 export { FAQ1 };
+
 
 
