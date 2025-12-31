d7b0b9c8de4670ed9134a6dbc1287b2dc5601999
hardik-cn <hardik.cantech@gmail.com>
2025-12-31 18:33:15 +0530

    Update page.tsx
    meta content updated

--- src/app/compute/kubernetes/page.tsx
+++ src/app/compute/kubernetes/page.tsx
@@ -1,5 +1,6 @@
 
 "use client";
+import type { Metadata } from "next";
 import Image from "next/image";
 import { motion } from "framer-motion";
 import ComputeNavbar from "../components/ComputeNavbar";
@@ -7,6 +8,36 @@ import HeroSection from "./components/HeroSection";
 import {ServicesSection} from "./components/ServicesSection"; 
 import FAQ1 from "../../components/FAQ1";
 import Footer from "../../components/Footer";
+
+export const metadata: Metadata = {
+  title: "Kubernetes Hosting as a Service | Cantech Cloud",
+  description: "Run containerized applications with Kubernetes Hosting and Kubernetes as a Service. Fully managed Kubernetes-as-a-Service for secure, scalable deployments.",
+  keywords: [
+    "Kubernetes",
+    "Kubernetes Hosting",
+    "Kubernetes as a Service",
+  ],
+   
+  openGraph: {
+    title: "Kubernetes Hosting as a Service | Cantech Cloud",
+    siteName: "Cantech Cloud",
+    url: "https://www.cantech.cloud/compute/kubernetes",
+    description: "Run containerized applications with Kubernetes Hosting and Kubernetes as a Service. Fully managed Kubernetes-as-a-Service for secure, scalable deployments.",
+    type: "website",
+    images: [
+      {
+        url: "/assets/images/og_image.png",
+        width: 1200,
+        height: 630,
+        alt: "Cantech Cloud",
+      },
+    ],
+  },
+
+  alternates: {
+    canonical: "https://www.cantech.cloud/compute/kubernetes",
+  },
+};
 
 export default function KubernetesPage()
 {
